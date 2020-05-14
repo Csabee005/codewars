d4bbaf6c9da6df4e0f829081e78d092179d1407b
Magyar Csaba <csabee005@gmail.com>
2020-05-14 18:06:27 +0200

    Added gitignore and first exercise

---
+++ array-split/src/main/com/codewars/training/Split/ArraySplitter.java
@@ -0,0 +1,10 @@
+package main.com.codewars.training.Split;
+
+public class ArraySplitter {
+    public static int[] splitAndAdd(int[] numbers, int n) {
+        for (int i = 0; i < n; i++) {
+            
+        }
+        return new int[0];
+    }
+}
---
+++ array-split/src/test/com/codewars/training/TestArraySplitter.java
@@ -0,0 +1,59 @@
+package test.com.codewars.training;
+
+import main.com.codewars.training.Split.ArraySplitter;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestArraySplitter {
+
+    @Test
+    public void splitAndAdd() throws Exception {
+
+        int[] expected = new int[]{5,10};
+        int[] input = ArraySplitter.splitAndAdd(new int[]{1,2,3,4,5},2);
+
+        assertEquals(Arrays.toString(expected), Arrays.toString(input));
+
+        expected = new int[]{15};
+        input = ArraySplitter.splitAndAdd(new int[]{1,2,3,4,5},3);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(input));
+
+        expected = new int[]{15};
+        input = ArraySplitter.splitAndAdd(new int[]{15},3);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(input));
+
+        expected = new int[]{183, 125};
+        input = ArraySplitter.splitAndAdd(new int[]{32,45,43,23,54,23,54,34},2);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(input));
+
+        expected = new int[]{32,45,43,23,54,23,54,34};
+        input = ArraySplitter.splitAndAdd(new int[]{32,45,43,23,54,23,54,34},0);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(input));
+
+        expected = new int[]{305, 1195};
+        input = ArraySplitter.splitAndAdd(new int[]{3,234,25,345,45,34,234,235,345},3);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(input));
+
+        expected = new int[]{1040, 7712};
+        input = ArraySplitter.splitAndAdd(new int[]{3,234,25,345,45,34,234,235,345,34,534,45,645,645,645,4656,45,3},4);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(input));
+
+        expected = new int[]{79327};
+        input = ArraySplitter.splitAndAdd(new int[]{23,345,345,345,34536,567,568,6,34536,54,7546,456},20);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(input));
+
+    }
+
+}
+
