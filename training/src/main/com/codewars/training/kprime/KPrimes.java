9b7b5ea3ba1a434f944aedc6c023523070af0bd0
Csaba_Magyar <csabee005@gmail.com>
2020-11-09 23:20:12 +0100

    What is the opposite of multiplication again?...

--- training/src/main/com/codewars/training/kprime/KPrimes.java
+++ training/src/main/com/codewars/training/kprime/KPrimes.java
@@ -2,26 +2,14 @@ package com.codewars.training.kprime;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.LongStream;
 
 public class KPrimes {
 
     public static long[] countKprimes(int k, long start, long end) {
-        List<Long> kPrimeList = new ArrayList<>();
-        for (long i = start; i < end; i++) {
-            PrimeCounter primeCounter = new PrimeCounter(i, k);
-            if (primeCounter.countDivisors() == k) {
-                kPrimeList.add(i);
-            }
-        }
-        return convertToArray(kPrimeList);
-    }
-
-    private static long[] convertToArray(List<Long> kPrimeList) {
-        long[] kPrimes = new long[kPrimeList.size()];
-        for (int i = 0; i < kPrimeList.size(); i++) {
-            kPrimes[i] = kPrimeList.get(i);
-        }
-        return kPrimes;
+        return LongStream.range(start, end + 1)
+                .filter(number -> new PrimeCounter(number, k).countDivisors() == k)
+                .toArray();
     }
 
     public static int puzzle(int s) {
--- training/src/main/com/codewars/training/kprime/PrimeCounter.java
+++ training/src/main/com/codewars/training/kprime/PrimeCounter.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeCounter {
-    private final long numberToCheck;
     private final long kPrimeLevel;
+
+    private long numberToCheck;
 
     public PrimeCounter(long numberToCheck, int kPrimeLevel) {
         this.numberToCheck = numberToCheck;
@@ -13,21 +14,17 @@ public class PrimeCounter {
     }
 
     public int countDivisors() {
-        int numberOfDivisors = 0;
-        List<Long> divisors = new ArrayList<>();
-        for (int i = 1; i < numberToCheck / 2 + 1; i++) {
-            if (numberToCheck %i == 0 && !divisors.contains(i) && !divisors.contains(numberToCheck/i)) {
-                numberOfDivisors++;
-                divisors.add(Integer.toUnsignedLong(i));
-                long power = kPrimeLevel;
-                while (Math.pow(i,power) != numberToCheck && numberToCheck%Math.pow(i,power) == 0 && i != 1) {
-                    numberOfDivisors++;
-                    divisors.add(Integer.toUnsignedLong((int) Math.pow(i,power)));
-                    power++;
-                }
+        int primeLevel = 0;
+        for (long i = 2; i < Math.sqrt(numberToCheck) + 1; ++i) {
+            while (numberToCheck % i == 0) {
+                primeLevel++;
+                numberToCheck /= i;
             }
         }
-        return numberOfDivisors;
+        if (numberToCheck > 1) {
+            primeLevel++;
+        }
+        return primeLevel;
     }
 
 }
